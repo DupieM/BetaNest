feec085a2ec2fb75e634e74750fb5b3607585f1d
DupieM <221326@virtualwindow.co.za>
2026-03-30 22:27:11 +0200

    started coding the login and sign up pages

--- betanest/src/Pages/authentication/login.js
+++ betanest/src/Pages/authentication/login.js
@@ -4,8 +4,32 @@ function LoginPage() {
   return (
     <div className="App">
       <p>
-        Login Page
+        Login Page bb
       </p>
+
+      <div className="form-group">
+        <label className="labels">Full Name</label> <br />
+        <input
+          type="text"
+          required
+        />
+
+        <br />
+
+        <label className="labels">Password</label> <br />
+        <input
+          type="password"
+          required
+        />
+      </div>
+
+      <br />
+
+      <button type="submit" className="submit-button">
+        Login
+      </button>
+
+
     </div>
   );
 }
--- betanest/src/Pages/authentication/signup.js
+++ betanest/src/Pages/authentication/signup.js
@@ -6,6 +6,39 @@ function SignupPage() {
       <p>
         Signup Page
       </p>
+
+      <div className="form-group">
+        <label className="labels">Full Name</label> <br />
+        <input
+          type="text"
+          required
+        />
+
+        <br />
+
+        <label className="labels-two">Email</label> <br />
+        <input
+          type="email"
+          required
+        />
+
+        <br />
+
+        <label className="labels">Password</label> <br />
+        <input
+          type="password"
+          required
+        />
+      </div>
+
+      <br />
+
+      <button type="submit" className="submit-button">
+        Sign Up
+      </button>
+
+      
+
     </div>
   );
 }
